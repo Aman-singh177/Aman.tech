aaf15d17633c9bea3d9d35e6087b4dced02357b8
Aman-singh177 <amansingh17730@gmail.com>
2025-12-17 14:39:10 +0530

    Home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,12 +3,21 @@ import './Home.css'
 
 const Home = () => {
   return (
-    <div className='navbar'>
-      <div className='navbar-name'>Aman Singh</div>
-      <div className='navbar-right'>
-        <p>About</p>
-        <p>Projects</p>
-        <p>Contact</p>
+    <div className='home'> 
+      <div className="detail">
+        <p className='greet'>Hello, I'm</p>
+        <p className='name'>Aman Singh</p>
+        <p className='role'>Full-Stack Developer</p>
+        <div className='btns'>
+          <button className='cv btn'>Download CV</button>
+          <button className="contact btn">Contact info</button>
+        </div>
+        <div>
+
+        </div>
+      </div> 
+      <div className="photo">
+        <img src="https://avatars.githubusercontent.com/u/64252445?v=4" alt="Aman Singh" />
       </div>
     </div> 
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className='navbar'>
+      <div className='navbar-name'>Aman Singh</div>
+      <div className='navbar-right'>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </div>
+    </div> 
+    </div>
+  )
+}
+
+export default Navbar
