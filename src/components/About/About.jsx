30155cf144c22956097c61c761750dfdf695ce70
Aman-singh177 <amansingh17730@gmail.com>
2025-12-19 17:22:34 +0530

    Added About Me Section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import './About.css'
+import EducationIcon from '../../assets/Education.png'
 
 const About = () => {
   return (
-    <div>
-      
+    <div className='main'>
+      <div className='head'>
+        <p>Get to Know More</p>
+        <h1 className=''>About Me</h1>
+      </div>
+      <div className='detail-about'>
+        <div>
+          <img src='https://i.pinimg.com/736x/da/97/5d/da975d71539c035ac0261b015228ba0d.jpg' alt='img' className='image'/>
+        </div>
+        <div className='info-about'>
+          <div className='cards'>
+            <div className='expirence-card'>
+              <img src={EducationIcon} alt='Experience' className='about-icon'/>
+              <p className='title'>Experience</p>
+              <p className='clr'>1.5 +years</p>
+              <p className='clr'>Mern Stack Devlopment</p>
+            </div>
+            <div className='education-card'>
+              <img src={EducationIcon} alt='Education' className='about-icon'/>
+              <p className='title'>Education</p>
+              <p className='clr'>B.Tech.(Undergrad)</p>
+              <p className='clr'>Lovely Professional University</p>
+            </div>
+          </div>
+          <div>
+            <p>
+              MERN Stack Developer proficient in MongoDB, Express.js, React.js, and Node.js, with
+              experience in building scalable and efficient web applications. Strong problem-solving
+              skills and a passion for creating innovative solutions.
+            </p> 
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -13,18 +13,18 @@ const Home = () => {
     <div className='main'> 
       <div className='home'>
         <div className="detail">
-        <p className='greet'>Hello, I'm</p>
-        <p className='name'>Aman Singh</p>
-        <p className='role'>Full-Stack Developer</p>
-        <div className='btns'>
-          <button className='cv btn'>Download CV</button>
-          <button className="contact btn">Contact info</button>
-        </div>
-        <div className='icons'>
-          <a href='https://www.linkedin.com/in/aman-singh-54435b252/' target='_blank' rel='noreferrer'>
-            <img src={linkedinIcon} alt='LinkedIn' className='icon'/>
-          </a>
-          <a href='https://github.com/Aman-singh177' target='_blank' rel='noreferrer'>
+          <p className='greet'>Hello, I'm</p>
+          <p className='name'>Aman  Singh</p>
+          <p className='role'>Full-Stack Developer</p>
+          <div className='btns'>
+            <button className='cv btn'>Download CV</button>
+            <button className="contact btn">Contact info</button>
+          </div>
+          <div className='icons'>
+            <a href='https://www.linkedin.com/in/aman-singh-54435b252/' target='_blank' rel='noreferrer'>
+              <img src={linkedinIcon} alt='LinkedIn' className='icon'/>
+            </a>
+            <a href='https://github.com/Aman-singh177' target='_blank' rel='noreferrer'>
             <img src={githubIcon} alt='LinkedIn' className='icon' />
             </a>
           </div>
@@ -34,19 +34,23 @@ const Home = () => {
         </div>
       </div>
       <div className='tech-stack'>
-        <div><p>Tech-Stack</p></div>
+        <div className='word'>
+          <p>Tech-Stack</p> 
+          <div className='line'></div>
+        </div>
+
         <div className='tech-icons'>
-          <div>
+          <div className='row'>
             <img src={html} alt="HTML" className='tech-icon' />
             <img src={css} alt="CSS" className='tech-icon' />
             <img src={js} alt="JavaScript" className='tech-icon' />
           </div>
-          <div>
+          <div className='row'>
             <img src={express} alt="Express.js" className='tech-icon' />
             <img src={mongo} alt="MongoDB" className='tech-icon' />
             <img src={js} alt="JavaScript" className='tech-icon' />
           </div>
-          <div>
+          <div className='row'>
             <img src={express} alt="Express.js" className='tech-icon' />
             <img src={mongo} alt="MongoDB" className='tech-icon' />
             <img src={js} alt="JavaScript" className='tech-icon' />
