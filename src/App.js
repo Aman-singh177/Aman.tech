bb6ad56a947029c2f88625f57bdfc9292cca4683
Aman-singh177 <amansingh17730@gmail.com>
2025-12-20 18:03:21 +0530

    project section added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import About from './components/About/About';
 import Home from './components/Home/Home';
 import Navbar from './components/Navbar/Navbar';
+import Project from './components/Projects/Project';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Home/>
       <About/>
+      <Project/>
     </div>
   );
 }
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,10 +1,48 @@
 import React from 'react'
 import './Project.css'
+import giti from '../../assets/giti.png'
 
 const Project = () => {
   return (
-    <div>
-      
+    <div className='project'>
+      <div className='project-text'>
+        <p>Know About My Skills</p>
+        <p className='project-title'>Projects</p>
+      </div>
+      <div>
+        <div className='pcard1'>
+          <div className='pcard-content'>
+
+          </div>
+          <div className='description'>
+            <h2>Real Time Communication System</h2>
+            <p>Designed and implemented a real-time communication platform with WebRTC and Socket.io, enabling 
+              secure peer-to-peer video calls. Optimized connection lifecycle and performance for a smooth 
+              user experience.
+            </p>
+            <div className='desc-stack'>
+              <div className='s1'>
+                <p>HTML</p>
+                <p>CSS</p>
+                <p>Javascript</p>
+                <p>React.js</p> <br/>
+              </div>
+              <div className='s2'>
+                <p>Node.js</p>
+                <p>Express.js</p>
+                <p>Socket.io</p>
+                <p>Webrtc</p>
+              </div>
+            </div>
+            <div>
+              <div className='code-btn'>
+                <p>Code</p> 
+                <img src={giti} alt=''  />
+              </div> 
+            </div>
+          </div>
+        </div> 
+      </div>
     </div>
   )
 }
